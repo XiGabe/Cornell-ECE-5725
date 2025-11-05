18c499b09bfd026a23451d9606857b6dee05c5aa
hongxi <hc2325@cornell.edu>
2025-11-05 13:50:20 -0500

    1st commit for proj

---
+++ PROJ/build/CMakeFiles/YoloFastestV2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../YoloFastestV2"
+  "../YoloFastestV2.pdb"
+  "CMakeFiles/YoloFastestV2.dir/mainFV2.cpp.o"
+  "CMakeFiles/YoloFastestV2.dir/yolo-fastestv2.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/YoloFastestV2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
